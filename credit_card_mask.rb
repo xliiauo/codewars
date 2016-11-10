2a6a85b15db7b0211486a4f70e4b3047d5d9b77d
Xiao Liu <liu.xiao@me.com>
2016-11-10 17:12:06 +0000

    Add regex solution

--- credit_card_mask.rb
+++ credit_card_mask.rb
@@ -1,7 +1,12 @@
 #String[-n..-m] is especial useful when handling strings
-
 def maskify cc
 	length = cc.length
 	return cc unless length > 4
   '' << ('#' * (cc.length - 4) + cc[-4..-1])
+end
+
+
+#String.gsub(pattern, replacement), regex is powerful
+def maskify_gsub cc
+  cc.gsub(/.(?=....)/, '#')
 end
